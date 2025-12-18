7849ad4b3b103c9bd1ded3c4c2a6242dab2d6ae6
Tharsa7 <tharsatharsi2000@gmail.com>
2025-12-18 21:08:58 +0530

    Removed non-functional arrow button from case studies

--- client/src/pages/CaseStudies.jsx
+++ client/src/pages/CaseStudies.jsx
@@ -323,11 +323,6 @@ const CaseStudies = () => {
                                             </div>
                                         )}
 
-                                        <button className="case-arrow-btn">
-                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                                <path d="M5 12h14M12 5l7 7-7 7" />
-                                            </svg>
-                                        </button>
                                     </div>
                                 </div>
                             ))}
